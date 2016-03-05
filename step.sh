4e74a5f8f13896909b00fca2ce47ea77c9087ca2
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2016-03-05 20:34:59 +0100

    Install gems quietly

--- step.sh
+++ step.sh
@@ -2,7 +2,7 @@
 
 this_dir=$( cd $( dirname ${BASH_SOURCE[0]} ) && pwd )
 
-bundle install --gemfile $this_dir/Gemfile
+bundle install --quiet --gemfile $this_dir/Gemfile
 bundle exec $this_dir/bin/slack-step
 
 exit $?
