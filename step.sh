ee5a7a1c5843738994438e10a6ba859a5d8cd4ad
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2016-03-06 13:30:12 +0100

    Move gem install to shell script

--- step.sh
+++ step.sh
@@ -2,6 +2,8 @@
 
 this_dir=$( cd $( dirname ${BASH_SOURCE[0]} ) && pwd )
 
+gem install bundler --quiet --no-rdoc --no-ri
+
 bundle install --quiet --gemfile $this_dir/Gemfile
 bundle exec $this_dir/bin/slack-step
 
