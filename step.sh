cdf3e940601fb0967a05d493f7c083b59806e5c8
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2016-03-06 13:57:38 +0100

    Gem install silently

--- step.sh
+++ step.sh
@@ -2,7 +2,7 @@
 
 step_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 
-gem install bundler --quiet --no-rdoc --no-ri
+gem install bundler --quiet --silent --no-rdoc --no-ri
 
 (cd $step_dir && bundle install --quiet)
 (cd $step_dir && bundle exec ./bin/slack-step)
