cacbe2951fd05eada775b492356af1dcaffd16a1
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2016-03-09 13:06:46 +0100

    Do not install bundler if it is already installed

--- step.sh
+++ step.sh
@@ -2,7 +2,7 @@
 
 step_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 
-gem install bundler --quiet --no-rdoc --no-ri > /dev/null
+which bundle > /dev/null || gem install bundler --quiet --no-rdoc --no-ri > /dev/null
 
 (cd $step_dir && bundle install --quiet)
 (cd $step_dir && bundle exec ./bin/slack-step)
